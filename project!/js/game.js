6662c06a06b0242608b6e1ecbfdd9eb68c144017
shirShira <92361467+shirShira@users.noreply.github.com>
2021-12-11 18:55:50 +0200

    Add files via upload

---
+++ project!/js/game.js
@@ -0,0 +1,32 @@
+let x = Math.floor((Math.random() * 10) + 1), y = Math.floor((Math.random() * 10) + 1);
+let result = x + y;
+document.getElementById('exe').innerHTML = `${x} + ${y} =`;
+let solut = document.getElementById('solution').value;
+
+console.log(solut==result);
+// while(document.getElementById('click'))
+// {
+//     if (solut == result) {
+//         document.getElementById('exe').innerHTML = "תשובה נכונה";
+//     }
+// }
+let allCards = document.querySelectorAll('.card');
+allCards.forEach((card) => card.addEventListener('click', checkCard));
+
+function checkCard(eventArgs) {
+    console.log(eventArgs.target.id);
+    eventArgs.target.classList.add('hide');
+}
+// let op = alert(document.body.dataset.operation);
+// btn.addEventListener('click', () => {
+//     alert('button clicked, registed with addEventListener');
+// });
+
+
+// switch(op){
+//     case "*": result =x*y;
+//     case "/": result =x/y;
+//     case "+": result =x+y;
+//     case "-": result =x-y;
+// }
+
